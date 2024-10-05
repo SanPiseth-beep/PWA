094316e55c23da2e7280c053dd92c62823bbdb1f
SanPiseth-beep <81914277+SanPiseth-beep@users.noreply.github.com>
2024-10-05 15:58:56 -0500

    Add UI initialization for side navigation

---
+++ js/ui.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Sidenav Initialization
+    const menus = document.querySelector(".sidenav");
+    M.Sidenav.init(menus, { edge: "right" });
+  });
