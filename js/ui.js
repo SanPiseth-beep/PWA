926a2d770e1a114736ac0b26af9b534ef863f15d
SanPiseth-beep <81914277+SanPiseth-beep@users.noreply.github.com>
2024-10-05 16:52:00 -0500

    update pages

--- js/ui.js
+++ js/ui.js
@@ -3,3 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const menus = document.querySelector(".sidenav");
     M.Sidenav.init(menus, { edge: "right" });
   });
+
+document.getElementById('toggle-btn').addEventListener('click', function() {
+  var textContent = document.getElementById('text-content');
+  var toggleBtn = document.getElementById('toggle-btn');
+
+  // Toggle the 'truncate' class to show or hide the full text
+  textContent.classList.toggle('truncate');
+
+  // Change button text based on the current state
+  if (textContent.classList.contains('truncate')) {
+    toggleBtn.textContent = 'Show More';
+  } else {
+    toggleBtn.textContent = 'Show Less';
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
